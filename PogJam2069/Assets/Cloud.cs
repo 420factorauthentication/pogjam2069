e1b0dbb0d8d16152f5db21c284253786ed97f464
AAYFlower <44514581+AAYFlower@users.noreply.github.com>
2020-12-05 04:12:06 -0600

    woahart

---
+++ PogJam2069/Assets/Cloud.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3 CloudDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position += CloudDirection * step;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    
+        if (collision.gameObject.tag == "CloudDestroy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
