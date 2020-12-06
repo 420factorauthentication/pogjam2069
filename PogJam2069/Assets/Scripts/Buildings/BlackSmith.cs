2bc3fa454adeec4165a0717f53210ce48f50185f
420factorauthentication <420factorauthentication@gmail.com>
2020-12-06 09:53:19 -0800

    fix event

--- PogJam2069/Assets/Scripts/Buildings/BlackSmith.cs
+++ PogJam2069/Assets/Scripts/Buildings/BlackSmith.cs
@@ -106,6 +106,6 @@ public class BlackSmith : MonoBehaviour, IBuilding
             "",
             null
         );
-        SurpriseManager.Smanager.PostSurprise(surprise1, false);
+        SurpriseManager.Smanager.PostSurprise(surprise1, true);
     }
 }
